ae50cb532fe12f6b528b334ef4d5ac44df428c32
19-Evgen-86 <95378553+19-Evgen-86@users.noreply.github.com>
2022-03-19 11:56:56 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+import utils as db
+
+app = Flask(__name__)
+
+
+@app.route('/movie/<title>', methods=['GET'])
+def get_film_by_title(title):
+    response = db.search_by_title(title)
+    return jsonify(response)
+
+
+@app.route('/movie/<int:start>/to/<int:end>', methods=['GET'])
+def get_film_by_year_to_year(start, end):
+    response = db.search_year_to_year(start, end)
+    return jsonify(response)
+
+
+@app.route('/rating/<rating>', methods=['GET'])
+def get_film_by_taring(rating):
+    response = db.search_by_rating(rating)
+    return jsonify(response)
+
+
+@app.route('/genre/<genre>', methods=['GET'])
+def get_film_by_genre(genre):
+    response = db.search_by_genre(genre)
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run()
